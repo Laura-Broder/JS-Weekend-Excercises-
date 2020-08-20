a1251ba95066212ac6a0779caa25ff7863985892
Laura Broder <laura.kokielov@gmail.com>
2020-08-20 14:40:13 +0300

    Ex.1

---
+++ Ex1.1-YesorNo.js
@@ -0,0 +1,7 @@
+// Ex1.1 - Yes or No
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+const YesOrNo = (bool) => (bool ? "Yes" : "No");
+
+console.log(YesOrNo(true));
+console.log(YesOrNo(false));
