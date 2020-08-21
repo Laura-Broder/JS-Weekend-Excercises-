b0e73fcda3d63064ddb917cbe66945c3760339f5
Laura Broder <laura.kokielov@gmail.com>
2020-08-21 15:43:23 +0300

    add new

---
+++ Ex6.3-organize_strings.js
@@ -0,0 +1,27 @@
+// Ex6.3-organize_strings
+
+// Take 2 strings s1 and s2 including only letters from a to z.
+// Return a new sorted string, the longest possible, containing distinct letters, each taken only once - coming from s1 or s2.
+
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+const longest = (s1, s2) => {
+  const joinedStr = s1.concat(s2);
+  let resultStr = [];
+  for (let char of joinedStr) {
+    if (!resultStr.includes(char)) resultStr = resultStr.concat(char);
+  }
+  return resultStr.sort().join("");
+};
+
+const a = "xyaabbbccccdefww";
+const b = "xxxxyyyyabklmopq";
+const c = "abcdefghijklmnopqrstuvwxyz";
+console.log(longest(a, b));
+console.log(longest(c, c));
